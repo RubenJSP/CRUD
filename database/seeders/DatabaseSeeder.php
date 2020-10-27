ba30810d903a4b878d71252da12bed275ef933cd
RubenJSP <56006571+RubenJSP@users.noreply.github.com>
2020-10-27 13:24:51 -0700

    Actividad 27/10/2020

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CategorySeeder::Class);
+        $this->call(BookSeeder::Class);
         // \App\Models\User::factory(10)->create();
     }
 }
